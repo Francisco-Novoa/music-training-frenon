42782312662765ebe3438dd27c5b83fe5719a204
Francisco-Novoa <francisco.novoa.14@sansano.usm.cl>
2020-10-07 16:02:53 -0300

    chore/removed conflict

--- server.js
+++ server.js
@@ -1,37 +1,15 @@
-<<<<<<< HEAD
 const express = require("express");
-const cors = require("cors");
-const morgan = require("morgan");
-const helmet = require("helmet");
-const methodOverride = require("method-override");
-const bodyParser = require("body-parser");
-const app = express();
-
-app.use(cors({ credentials: true }));
-
-app.use(helmet());
-
-app.use(bodyParser.json());
-
-app.use(express.json());
-
-app.use(express.urlencoded({ extended: true }));
-
-app.use(morgan("tiny"));
-=======
-const express = require("express")
 const PORT = 5000;
 
-const { app } = require('./midlewares')
+const { app } = require("./midlewares");
 
+app.get("/", (req, res) => {
+  res.send(200, { message: "Bienvenido a Api Task" });
+});
 
-
-app.get('/', (req, res)=>{
-    res.send(200,{message: 'Bienvenido a Api Task'})
-})
-
-app.listen(PORT, () => { console.log(`Server is running... in port ${PORT}`) });
->>>>>>> 5b921d74b7c476063dc3dc79fa5bd27125a5d459
+app.listen(PORT, () => {
+  console.log(`Server is running... in port ${PORT}`);
+});
 
 app.listen(5000, () => {
   console.log("Server is running...");
