414ca5c1a965817aba746987e78749459a6fb37c
Francisco-Novoa <francisco.novoa.14@sansano.usm.cl>
2020-10-14 11:34:34 -0300

    feat/added middleware to handle 404 and added it in the middlewares index.js

--- midlewares/index.js
+++ midlewares/index.js
@@ -3,6 +3,7 @@ const cors = require("cors")
 const morgan = require("morgan")
 const helmet = require("helmet")
 const methodOverride = require("method-override")
+const unknownEndpoint = require("./unknownEndpoint")
 const bodyParser = require("body-parser")
 const mongoose = require('mongoose');
 
@@ -13,7 +14,9 @@ app.use(helmet());
 app.use(bodyParser.json());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.use(unknownEndpoint)
 app.use(morgan("tiny"));
+
 
 
 module.exports = { app }
