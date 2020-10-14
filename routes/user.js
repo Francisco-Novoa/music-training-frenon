eda09a5fba5ffcc3bdfb53f0207b830951251a13
Francisco-Novoa <francisco.novoa.14@sansano.usm.cl>
2020-10-14 13:07:28 -0300

    added router to user

--- routes/user.js
+++ routes/user.js
@@ -1,22 +1,23 @@
-const express = require('express')
-const app = express()
+const user = require('express').Router()
 
-app.get('/users', (req, res) => {
+user.get('/users', (req, res) => {
     res.send('GET request to the homepage')
 })
 
-app.get('/users/:id', (req, res) => {
+user.get('/users/:id', (req, res) => {
     res.send('GET request to the homepage')
 })
 
-app.post('/users', (req, res) => {
+user.post('/users', (req, res) => {
     res.send('POST request to the homepage')
 })
 
-app.put('/users/:id', (req, res) => {
+user.put('/users/:id', (req, res) => {
     res.send('PUT request to the homepage')
 })
 
-app.delete('/users/:id', (req, res) => {
+user.delete('/users/:id', (req, res) => {
     res.send('DELETE request to the homepage')
 })
+
+module.exports=user
