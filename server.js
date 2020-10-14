00fa45c09509d679934d06408c8fed4a4b4d3eb6
yanachernova <yana.chernova94@gmail.com>
2020-10-14 11:43:25 -0300

    chore: create routes for task

--- server.js
+++ server.js
@@ -1,12 +1,7 @@
 const express = require("express");
 const PORT = 5000;
 
-<<<<<<< HEAD
-const { app } = require('./midlewares')
-const db = require('./db/connection')
-=======
 const { app } = require("./midlewares");
->>>>>>> 6835e2621d83c397a2bb70209ff4ce544dc45c6e
 
 app.get("/", (req, res) => {
   res.send(200, { message: "Bienvenido a Api Task" });
