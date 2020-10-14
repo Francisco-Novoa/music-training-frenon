02915821792a83c01f5721a11acf88f1e4fc4c14
Francisco-Novoa <francisco.novoa.14@sansano.usm.cl>
2020-10-14 14:40:07 -0300

    feat/added mailer function

--- server.js
+++ server.js
@@ -8,14 +8,9 @@ const db = require('./db/connection')
 app.get("/", (req, res) => {
   res.send(200, { message: "Bienvenido a Api Task" });
 });
-app.use('/api', routes)
-app.listen(PORT, () => {
-  console.log(`Server is running... in port ${PORT}`);
-});
 
-app.get('/', (req, res)=>{
-    res.send(200,{message: 'Bienvenido a Api Task'})
-})
+app.use('/api', routes)
+
 app.use(unknownEndpoint)
 app.use(errorHandler)
 
