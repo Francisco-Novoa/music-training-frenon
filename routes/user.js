4cfb27c9be86eb8e6d15aa67972b887765c5b614
Francisco-Novoa <francisco.novoa.14@sansano.usm.cl>
2020-10-14 13:10:16 -0300

    fix: removed redundant url in router

--- routes/user.js
+++ routes/user.js
@@ -1,22 +1,22 @@
 const user = require('express').Router()
 
-user.get('/users', (req, res) => {
+user.get('/', (req, res) => {
     res.send('GET request to the homepage')
 })
 
-user.get('/users/:id', (req, res) => {
+user.get('/:id', (req, res) => {
     res.send('GET request to the homepage')
 })
 
-user.post('/users', (req, res) => {
+user.post('/', (req, res) => {
     res.send('POST request to the homepage')
 })
 
-user.put('/users/:id', (req, res) => {
+user.put('/:id', (req, res) => {
     res.send('PUT request to the homepage')
 })
 
-user.delete('/users/:id', (req, res) => {
+user.delete('/:id', (req, res) => {
     res.send('DELETE request to the homepage')
 })
 
